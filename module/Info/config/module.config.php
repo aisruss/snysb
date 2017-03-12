09e303aada04a9c34e5319fb52dd808f00bbdb96
ais_r <ais_r@192.168.1.126>
2017-03-12 15:42:39 +0000

    route

--- module/Info/config/module.config.php
+++ module/Info/config/module.config.php
@@ -1,50 +1,59 @@
 <?php
-return array(
-    'controllers' => array(
-        'invokables' => array(
+namespace Info;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\InfoController::class => InvokableFactory::class,
+        ],
+        'invokables' => [
             'Info\Controller\Info' => 'Info\Controller\InfoController',
-        ),
-    ),
-    'router' => array(
-        'routes' => array(
-            'info' => array(
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'info' => [
                 'type'    => 'Literal',
-                'options' => array(
+                'options' => [
                     // Change this to something specific to your module
                     'route'    => '/info',
-                    'defaults' => array(
+                    'defaults' => [
                         // Change this value to reflect the namespace in which
                         // the controllers for your module are found
                         '__NAMESPACE__' => 'Info\Controller',
                         'controller'    => 'Info',
                         'action'        => 'index',
-                    ),
-                ),
+                    ],
+                ],
                 'may_terminate' => true,
-                'child_routes' => array(
+                'child_routes' => [
                     // This route is a sane default when developing a module;
                     // as you solidify the routes for your module, however,
                     // you may want to remove it and replace it with more
                     // specific routes.
-                    'default' => array(
+                    'default' => [
                         'type'    => 'Segment',
-                        'options' => array(
+                        'options' => [
                             'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
+                            'constraints' => [
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
+                            ],
+                            'defaults' => [
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
             'Info' => __DIR__ . '/../view',
-        ),
-    ),
-);
+        ],
+    ],
+];
