b8c3800820b1a47eca1a15174bf79c428680b71c
ais_r <ais_r@DESKTOP-NT2GAR9>
2017-03-19 18:43:47 +0000

    all base files needed for contact

--- module/Info/src/Info/Controller/InfoController.php
+++ module/Info/src/Info/Controller/InfoController.php
@@ -26,6 +26,10 @@ class InfoController extends AbstractActionController
     {
         return new ViewModel();
     }
+    public function contactAction()
+    {
+        return new ViewModel();
+    }
 
     public function fooAction()
     {
